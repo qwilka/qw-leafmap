8900be097e476d54409d82cf1a90e4f9d853bf26
Stephen McEntee <stephenmce@gmail.com>
2020-01-27 19:46:23 +0000

    layer control working

--- qwgis/app.js
+++ qwgis/app.js
@@ -126,61 +126,57 @@ function makeGis(confData, id_number) {
     };
 
 
+    // for (let ii=0; ii<ftree.length; ii++) {
+    //   let toplevel = ftree[ii], mapObj, layer;
+    //   if (toplevel.type === "gis-folder-basemaps") {
+    //     for (let jj=0; jj<toplevel.children.length; jj++) {
+    //       let layerObj = toplevel.children[jj];
+    //       if (layerObj.type !== "gis-layer-basemap") continue;
+    //       // console.log("layerObj", layerObj);
+    //       let layer = createMapLayer(layerObj, map);
+    //       //console.log("layer", layer);
+    //       baseTree.children.push({ label: layerObj.title, layer: layer })
+    //       allMapLayers[layerObj.data.layerId] = layer;
+    //       console.log("layerObj.title", layerObj.title);
+    //       console.log("layerObj.selected", layerObj.selected);
+    //       if (layerObj.selected) {
+    //         console.log("default layer", layer);
+    //         map.addLayer(layer);
+    //       }
+    //       //if (jj==2) break;
+    //     }
+    //   }
+    // }
+
+
+    //let overlayTree = {label: 'overlays', children: []};
+    let overlayTree = [];
+
     for (let ii=0; ii<ftree.length; ii++) {
-      let toplevel = ftree[ii], mapObj, layer;
-      if (toplevel.type === "gis-folder-basemaps") {
-        for (let jj=0; jj<toplevel.children.length; jj++) {
-          let layerObj = toplevel.children[jj];
-          if (layerObj.type !== "gis-layer-basemap") continue;
-          // console.log("layerObj", layerObj);
-          let layer = addMapLayer(layerObj, map);
-          //console.log("layer", layer);
-          baseTree.children.push({ label: layerObj.title, layer: layer })
-          allMapLayers[layerObj.data.layerId] = layer;
-          console.log("layerObj.title", layerObj.title);
-          console.log("layerObj.selected", layerObj.selected);
-          if (layerObj.selected) {
-            console.log("default layer", layer);
-            map.addLayer(layer);
-          }
-          //if (jj==2) break;
-        }
+      let ftree_toplevel = ftree[ii], mapObj, layer;
+      console.log("ftree_toplevel.type", ftree_toplevel.type)
+      if (ftree_toplevel.type === "gis-folder-basemaps") {
+        baseTree = makeLayersTree(ftree_toplevel, map, allMapLayers);
+      } else if (ftree_toplevel.type.startsWith("gis-folder")) {
+        console.log("ftree_toplevel.title", ftree_toplevel.title)
+        overlayTree.push(makeLayersTree(ftree_toplevel, map, allMapLayers));
       }
     }
 
+    let hash = new L.Hash(map, allMapLayers);    
 
-    // let baseTree = {
-    //   label: 'World base maps &#x1f5fa;',
-    //   children: [
-    //       { label: GEBCO.title, layer: GEBCO.layer },
-    //       { label: 'white background', layer: WhiteBG }
-    //   ]
-    // };
-    // let overlayTree = {
-    //   label: 'overlays',
-    //   children: [
-    //       { label: GEBCO.title, layer: GEBCO.layer },
-    //       { label: 'blank', layer: WhiteBG }
-    //   ]
-    // };
-    let overlayTree = {label: 'overlays', children: []};
-
-    var layerCtl = L.control.layers.tree(baseTree, overlayTree, {
-      collapsed: true,
-      collapseAll: '<font color="#909090" size="2">(close all)</font>'  
+    let layerCtl = L.control.layers.tree(baseTree, overlayTree, {
+      collapseAll: '<font color="#909090" size="2">(close all)</font>',
+      collapsed: true 
     });
     layerCtl.addTo(map);
-    layerCtl.collapseTree(true);
-    layerCtl.collapseTree(true);
-    layerCtl.setPosition('topleft');
+    layerCtl.setPosition('topleft');   
+    layerCtl.collapseTree(false);
+    layerCtl.collapseTree(true); 
 
-    //  console.log("allMapLayers", allMapLayers);
-    var hash = new L.Hash(map, allMapLayers);
+  }
 
-}
-
-function addMapLayer(layerObj, mapref) {
-  //console.log("addLayer layerObj", layerObj);
+function createMapLayer(layerObj) {
   let layer;
   switch(layerObj.data.layerType) {
     // case "geojson":
@@ -198,4 +194,39 @@ function addMapLayer(layerObj, mapref) {
   // console.log("addLayer layerStamp", layerStamp);
   // console.log("addLayer layer", layer);
   return layer;
+}
+
+
+function makeLayersTree(ftree_folder, mapref, allMapLayers) {
+  let layerTreeChildren = [];
+  for (let jj=0; jj<ftree_folder.children.length; jj++) {
+    let layerObj = ftree_folder.children[jj];
+    //console.log("layerObj.title", layerObj.title);
+    //console.log("layerObj.type", layerObj.type);
+    if (layerObj.type.startsWith("gis-layer")) {
+      // console.log("layerObj", layerObj);
+      let layer = createMapLayer(layerObj);
+      //console.log("layer", layer);
+      layerTreeChildren.push({ label: layerObj.title, layer: layer });
+      if (layerObj.data.layerId) {
+        allMapLayers[layerObj.data.layerId] = layer;
+      }
+      // console.log("layerObj.title", layerObj.title);
+      // console.log("layerObj.selected", layerObj.selected);
+      if (layerObj.selected) {
+        //console.log("default layer", layer);
+        mapref.addLayer(layer);
+      }
+    } else if (layerObj.type.startsWith("gis-folder")) {
+      //console.log("recursive layerObj.type gis-folder", layerObj.type);
+      //let layerTreeFolder = {label:layerObj.title, children:[]};
+      let obj = makeLayersTree(layerObj, mapref, allMapLayers);
+      layerTreeChildren.push(obj);
+    };
+  }
+  let layerTreeObj = {
+    label: ftree_folder.title,
+    children: layerTreeChildren
+  }
+  return layerTreeObj;
 }
