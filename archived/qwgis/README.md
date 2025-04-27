94044d785cfc02e1b69d1b42449779089405bb9f
Stephen McEntee <stephenmce@gmail.com>
2025-04-27 19:29:22 +0100

    archived qwgis

---
+++ archived/qwgis/README.md
@@ -0,0 +1,8 @@
+# qw-map (archived)
+Simple webmap app based on [Leaflet.js](https://leafletjs.com/), application development stopped in 2020, configuation updated in 2025.
+
+
+https://qwilka.github.io/gis/1/#5/53.980/-7.300/g1
+
+
+
---
+++ archived/qwgis/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCdnPlugin = require('webpack-cdn-plugin');
+
+// const webpack = require('webpack');
+//   new webpack.ProvidePlugin({
+//     "window.L": "leaflet"
+//   }),
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  mode,
+  output: {
+    path: path.join(__dirname, 'gis/assets'),
+    publicPath: '/gis/assets',
+    filename: 'qwgis.js',
+  },
+  externals: {
+    'leaflet': 'L'
+  },
+  module: {
+    rules: [
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      filename: '../1/index.html',
+      template: "./index.html"
+    }), // output file relative to output.path
+    new WebpackCdnPlugin({
+      modules: [
+        { 
+          name: 'leaflet',
+          style: '',
+          path: 'dist/leaflet.js'
+        }
+      ],
+      publicPath: '/node_modules', // override when prod is false
+    }),
+  ],
+};
